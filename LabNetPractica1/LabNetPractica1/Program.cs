7ad925be26e96bc20661996cf6d31bb09a6e39b5
Ana Paula Brajús <anapaulabrajus2001@gmail.com>
2023-08-18 15:18:19 -0300

    Se agrega validaciones y color
    Se agrego validaciones de S/N
    Se agrego color
    Capaz habria que implementar un metodo y enviar parametros porque se esta repitiendo codigo

--- LabNetPractica1/LabNetPractica1/Program.cs
+++ LabNetPractica1/LabNetPractica1/Program.cs
@@ -13,12 +13,19 @@ namespace LabNetPractica1
         {
             List<TransportePublico> transportes = new List<TransportePublico>();
 
+            Console.WriteLine("**************************************");
+            Console.WriteLine("*  Bienvenido a la App de Transporte  *");
+            Console.WriteLine("**************************************");
+            Console.WriteLine();
+
             string respuesta;
 
             do
             {
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.Write("¿Desea ingresar un Taxi (T) o un Ómnibus (O)? ");
                 string tipoTransporte = Console.ReadLine();
+                Console.ResetColor();
 
                 if (tipoTransporte.Equals("T", StringComparison.OrdinalIgnoreCase))
                 {
@@ -32,7 +39,9 @@ namespace LabNetPractica1
                         }
                         else
                         {
+                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("Cantidad de pasajeros inválida. Intente de nuevo.");
+                            Console.ResetColor();
                         }
                     }
                     transportes.Add(new Taxi(pasajerosTaxi));
@@ -49,49 +58,112 @@ namespace LabNetPractica1
                         }
                         else
                         {
+                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("Cantidad de pasajeros inválida. Intente de nuevo.");
+                            Console.ResetColor();
                         }
                     }
                     transportes.Add(new Omnibus(pasajerosOmnibus));
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Tipo de transporte no válido. Intente de nuevo.");
+                    Console.ResetColor();
                 }
 
-                Console.Write("¿Desea ingresar otro transporte? (S/N): ");
-                respuesta = Console.ReadLine();
+                while (true)
+                {
+                    Console.Write("¿Desea ingresar otro transporte? (S/N): ");
+                    respuesta = Console.ReadLine();
+                    if (respuesta.Equals("S", StringComparison.OrdinalIgnoreCase) || respuesta.Equals("N", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Respuesta inválida. Intente de nuevo.");
+                        Console.ResetColor();
+                    }
+                }
             }
             while (respuesta.Equals("S", StringComparison.OrdinalIgnoreCase));
 
             Console.WriteLine();
-            Console.WriteLine("Listas de transportes:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("**************************************");
+            Console.WriteLine("*        Listas de Transportes        *");
+            Console.WriteLine("**************************************");
+            Console.ResetColor();
             Console.WriteLine();
 
             int numeroTaxi = 1;
             int numeroOmnibus = 1;
 
-            Console.WriteLine("Listas de taxis:");
+            bool hayTaxis = false;
+            bool hayOmnibus = false;
+
             foreach (var transporte in transportes)
             {
                 if (transporte is Taxi)
                 {
+                    if (!hayTaxis)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("**************************************");
+                        Console.WriteLine("*            Listas de Taxis          *");
+                        Console.WriteLine("**************************************");
+                        Console.ResetColor();
+                        hayTaxis = true;
+                    }
+
                     Console.WriteLine($"Taxi {numeroTaxi}: {transporte.GetPasajeros()} pasajeros");
                     numeroTaxi++;
                 }
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Listas de omnibus:");
-            foreach (var transporte in transportes)
-            {
-                if (transporte is Omnibus)
+                else if (transporte is Omnibus)
                 {
+                    if (!hayOmnibus)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("**************************************");
+                        Console.WriteLine("*         Listas de Ómnibus           *");
+                        Console.WriteLine("**************************************");
+                        Console.ResetColor();
+                        hayOmnibus = true;
+                    }
+
                     Console.WriteLine($"Ómnibus {numeroOmnibus}: {transporte.GetPasajeros()} pasajeros");
                     numeroOmnibus++;
                 }
             }
+
+            if (!hayTaxis)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("**************************************");
+                Console.WriteLine("*            Listas de Taxis          *");
+                Console.WriteLine("**************************************");
+                Console.ResetColor();
+                Console.WriteLine("No se han ingresado taxis todavía.");
+            }
+
+            if (!hayOmnibus)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("**************************************");
+                Console.WriteLine("*         Listas de Ómnibus           *");
+                Console.WriteLine("**************************************");
+                Console.ResetColor();
+                Console.WriteLine("No se han ingresado ómnibus todavía.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("¡Gracias por usar nuestra App de Transporte!");
             Console.ReadKey();
         }
     }
 }
+
+
+
