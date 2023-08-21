e6e205699689973621a14fc0b2ad6abbef587d68
Ana Paula Brajús <anapaulabrajus2001@gmail.com>
2023-08-20 23:24:21 -0300

    Se realizo ejercicio 1, volver a mirar!!!

---
+++ LabNetPractica2/LabNetPractica2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingrese un valor");
+                int divisor=int.Parse(Console.ReadLine());
+
+                if (divisor==0)
+                {
+                    throw new DivisonByZeroException("Division por cero no permitida");
+                }
+            }
+            catch (DivisonByZeroException ex)
+            {
+                Console.WriteLine("Error: "+ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error general: "+ ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Operacion completada");
+                Console.ReadKey();
+            }
+        }
+    }
+}
