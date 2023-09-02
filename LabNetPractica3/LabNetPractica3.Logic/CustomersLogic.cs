089615cac7affcb79604becd22abc2fb300b774c
Ana Paula Brajús <anapaulabrajus2001@gmail.com>
2023-09-01 22:42:08 -0300

    Se realizan ejercicios 1,2 3 y 4 Se crea la BD Se crea una solucion que se conecta con la BD con EF Se organiza la app en capas Se realiza una consulta simple de las entidades Categorias y Customers

---
+++ LabNetPractica3/LabNetPractica3.Logic/CustomersLogic.cs
@@ -0,0 +1,25 @@
+﻿using LabNetPractica3.Data;
+using LabNetPractica3.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica3.Logic
+{
+    public class CustomersLogic
+    {
+        private readonly NorthwindContext context;
+
+        public CustomersLogic()
+        {
+            context = new NorthwindContext();
+        }
+
+        public List<Customers> GetAll()
+        {
+            return context.Customers.ToList();
+        }
+    }
+}
---
+++ LabNetPractica3/LabNetPractica3/Program.cs
@@ -0,0 +1,33 @@
+﻿using LabNetPractica3.Entities;
+using LabNetPractica3.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CategoriesLogic categoriesLogic=new CategoriesLogic();
+            CustomersLogic customersLogic=new CustomersLogic();
+
+            foreach (Categories categorie in categoriesLogic.GetAll())
+            {
+                Console.WriteLine($"{categorie.CategoryName} - {categorie.Description}");
+            }
+
+            Console.WriteLine();
+
+            foreach (Customers customer in customersLogic.GetAll())
+            {
+                Console.WriteLine($"{customer.ContactName} - {customer.CompanyName}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
